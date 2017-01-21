9f92edb27d17998e291427e0062db6f18b30881f
jacopofar <jacopo1.farina@gmail.com>
2017-01-21 21:41:36 +0100

    first commit

---
+++ sample_generator/sample_generator.py
@@ -0,0 +1,56 @@
+from nltk import CFG
+from nltk import ChartParser
+import xxhash
+
+__all__ = ['SampleGenerator']
+
+class SampleGenerator:
+    def __init__(self, grammar):
+        self.grammar = CFG.fromstring(grammar)
+        parser = ChartParser(self.grammar)
+        self.gr = parser.grammar()
+
+    """
+    Deterministic *random* choice based on a seed
+    Parameters
+    ----------
+    choices : array
+        List of elements, it must have a length and the get operation
+    seed : str
+        A string used as seed to choose a random element
+
+    """
+    @staticmethod
+    def _deterministic_choice(choices, seed):
+        xh = xxhash.xxh32()
+        xh.update(seed)
+        return choices[xh.intdigest() % len(choices)]
+
+    """
+    Expand a nonterminal *randomly* based on a seed
+    Parameters
+    ----------
+    symbol : Nonterminal
+        The starting symbol
+    seed : str
+        A string used as seed to choose a random element
+    counter : int
+        depth of the recursion, used to operate the deterministic choice
+    """
+    def _produce(self, symbol, seed, counter):
+        words = []
+        productions = self.grammar.productions(lhs=symbol)
+        production = self._deterministic_choice(productions, seed + str(counter))
+        for sym in production.rhs():
+            counter += 1
+            if isinstance(sym, str):
+                words.append(sym)
+            else:
+                words.extend(self._produce(sym, seed, counter + 1))
+        return words
+
+    def generate(self, seed):
+        return self._produce(self.gr.start(), seed, 0)
+
+
+
---
+++ test_generator.py
@@ -0,0 +1,26 @@
+import pytest
+from sample_generator import sample_generator
+
+class TestGenerator:
+    def test_instantiate(self):
+        self.gen = sample_generator.SampleGenerator("""
+        S -> EXP
+        EXP -> DIGIT | EXP ' + ' EXP | EXP ' * ' EXP | FUN '(' EXP ')' | DIGIT '.' DIGIT
+        DIGIT -> '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'| DIGIT DIGIT
+        FUN -> 'sin'|'sqrt'
+        """)
+        # no assertion, just check it doesn't fail
+
+        with pytest.raises(ValueError):
+            # the ---> symbol is a syntax error
+            sample_generator.SampleGenerator("""
+                    S ---> EXP
+                    DIGIT -> '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'| DIGIT DIGIT
+                    """)
+
+    def check_determinism(self):
+        assert self.gen.generate('aaa') == self.gen.generate('aaa')
+        assert self.gen.generate('aasdf') == self.gen.generate('aasdf')
+
+        for x in range(10):
+            print(''.join(self.gen.generate('ser' + str(x))))
