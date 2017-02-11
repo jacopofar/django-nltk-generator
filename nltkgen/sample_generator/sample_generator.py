9c4b4a6d4968e0fdb6d72314eb54ae2ec58f9f37
jacopofar <jacopo1.farina@gmail.com>
2017-02-12 00:37:20 +0100

    process features in grammars

--- nltkgen/sample_generator/sample_generator.py
+++ nltkgen/sample_generator/sample_generator.py
@@ -1,4 +1,4 @@
-from nltk import FeatureEarleyChartParser
+from nltk import FeatureEarleyChartParser, Variable
 from nltk import grammar as nltkgr
 import xxhash
 
@@ -26,6 +26,20 @@ class SampleGenerator:
         xh = xxhash.xxh32()
         xh.update(seed)
         return choices[xh.intdigest() % len(choices)]
+    """
+    Checks that the left side of a production respects a feature dictionary.
+    Parameters
+    ----------
+    production : Production
+        the production to check
+    features : dict
+        the feature dictionary that the production LHS must not contradict
+    """
+    @staticmethod
+    def _respects(production, features):
+        return all(map(lambda feat:
+                       feat not in features or features[feat] == production.lhs()[feat],
+                       production.lhs().keys()))
 
     """
     Expand a nonterminal *randomly* based on a seed, stopping when reaching an excessive recursion
@@ -39,28 +53,48 @@ class SampleGenerator:
         depth of the recursion, used to operate the deterministic choice
     max_depth : int
         the maximum recursion depth, if reached
+    max_depth_token : str
+        the replacement string to use when reaching maximum depth
+    bound_features : dict
+        the features to consider bound when expanding this nonterminal
     """
-    def _produce(self, symbol, seed, counter, max_depth, max_depth_token):
+    def _produce(self, symbol, seed, counter, max_depth, max_depth_token, bound_features):
         words = []
-        productions = self.grammar.productions(lhs=symbol)
+        productions = list(filter(lambda p : self._respects(p, bound_features), self.grammar.productions(lhs=symbol)))
         production = self._deterministic_choice(productions, seed + str(counter))
+        # by picking a production, was a feature bound?
+        # For example, symbol had GEN=?x and a production lhs with GEN=m was picked
+        # in that case th value will be returner to the caller to bound the next nonterminal in its RHS
+        bound_variables = {}
+        for bound_candidate in production.lhs().keys():
+            print('symbol', symbol)
+            if isinstance(symbol[bound_candidate], Variable):
+                bound_variables[bound_candidate] = production.lhs()[bound_candidate]
         if counter > max_depth:
             # print(' --max recursion reached--')
             words.append(max_depth_token)
             return True, words
         # print('picked expansion ' + str(production) + ' for ' + str(symbol) + ' at counter ' + str(counter))
         is_truncated = False
+        bound_placeholders = {}
         for (i, sym) in enumerate(production.rhs()):
             if isinstance(sym, str):
                 words.append(sym)
             else:
-                trunc, extension = self._produce(sym, seed, counter + 1 + i,  max_depth, max_depth_token)
+                # if sym has a key like '?x' and that is in bound placeholders, set it as a bounded valued in recursive call
+                nonterminal_bound_feats = {}
+                for key, value in sym.items():
+                    if value in bound_placeholders.keys():
+                        nonterminal_bound_feats[key] = bound_placeholders[value]
+                trunc, extension, bounded_variables = self._produce(sym, seed, counter + 1 + i,  max_depth, max_depth_token, nonterminal_bound_feats)
+                for key, value in bounded_variables.items():
+                    bound_placeholders[sym[key]] = value
                 is_truncated = is_truncated or trunc
                 words.extend(extension)
-        return is_truncated, words
+        return is_truncated, words, bound_variables
 
     def generate(self, seed, max_depth=100, max_depth_token=' --max recursion reached-- '):
-        return self._produce(self.gr.start(), seed, 0, max_depth, max_depth_token)
+        return self._produce(self.gr.start(), seed, 0, max_depth, max_depth_token, {})
 
 
 
